e2d67d122fb2671b661f76e21aac407c5c4454a8
Saverio Morelli <saverio.morelli@outlook.it>
2022-06-11 10:08:56 +0200

    Improvements

--- get-word.py
+++ get-word.py
@@ -1,5 +1,28 @@
 from wiktionaryparser import WiktionaryParser
 import sys
+import json
+
+
+def correctSingleQuoteJSON(s):
+    rstr = ""
+    escaped = False
+
+    for c in s:
+
+        if c == "'" and not escaped:
+            c = '"'  # replace single with double quote
+
+        elif c == "'" and escaped:
+            rstr = rstr[:-1]  # remove escape character before single quotes
+
+        elif c == '"':
+            c = '\\' + c  # escape existing double quotes
+
+        escaped = (c == "\\")  # check for an escape character
+        rstr += c  # append the correct json
+
+    return rstr
+
 
 text_to_find = ''
 if len(sys.argv) > 1:
@@ -13,13 +36,13 @@ HTML1 = """<!DOCTYPE html>
     <title>""" + text_to_find + """</title>
     <style>
         p {
-            padding: 10px;
             box-sizing: border-box;
+            padding: 10px;
         }
         textarea{
+            box-sizing: border-box;
             width: 1496px;
             height: 88px;
-            min-height: 100px;
             box-sizing: border-box;
             padding: 5px;
             margin: 0px;
@@ -27,27 +50,31 @@ HTML1 = """<!DOCTYPE html>
             border: 1px solid gray;
             resize: vertical;
             padding-left: 60px;
+            min-height: 100px;
         }
         
         input {
+            box-sizing: border-box;
             width: 100%;
             height: auto;
-            min-height: 50px
-            box-sizing: border-box;
             padding: 5px;
             margin: 0px;
             border-radius: 10px;
             border: 1px solid gray;
             padding-left: 60px;
+            min-height: 28px
         }
 
         .position-relative {
+            box-sizing: border-box;
+            width: 100%;
             position: relative;
             margin-top: 20px;
             margin-bottom: 20px;
         }
 
         button {
+            box-sizing: border-box;
             position: absolute;
             top: 22px;
             left: 3px;
@@ -101,8 +128,8 @@ HTML3_2 = """
 """
 
 parser = WiktionaryParser()
-word = parser.fetch(text_to_find, 'english')
-parser.set_default_language('english')
+word = parser.fetch(text_to_find, "english")
+parser.set_default_language("english")
 '''parser.exclude_part_of_speech('noun')
 parser.include_relation('alternative forms')'''
 
@@ -111,16 +138,22 @@ if len(word) > 0:
 
     etymology = details["etymology"]
     pronunciation_text = details["pronunciations"]["text"]
+    british_found = False
     for item in pronunciation_text:
-        if ("(Received Pronunciation) IPA: " in item):
+        if "(Received Pronunciation) IPA: " in item:
             pronunciation_text = str(item).replace("(Received Pronunciation) IPA: ", "").split(",")[0]
+            british_found = True
+        if not british_found and "IPA: " in item:
+            pronunciation_text = str(item).replace("IPA: ", "").split(",")[0]
     # print(word)
     verb_or_noun = details["definitions"][0]["partOfSpeech"]
     definition = ""
     counter = 0
     for item in details["definitions"][0]["text"]:
         counter += 1
-        definition += str(counter) + ". " + str(item) + "\n"
+        if counter > 1:
+            definition += "\n"
+        definition += str(counter) + ". " + str(item)
 
     '''print(text_to_find)
     print(verb_or_noun)
@@ -133,7 +166,7 @@ if len(word) > 0:
 
         f.write(HTML3_1.replace("%value%", str(text_to_find)).replace("%id%", "word").replace("%label%", "Word"))
         f.write(HTML3_1.replace("%value%", str(verb_or_noun)).replace("%id%", "verb_nourn_etc").replace("%label%",
-                                                                                                      "Verb, noun, ..."))
+                                                                                                        "Verb, noun, ..."))
         f.write(HTML3_1.replace("%value%", str(pronunciation_text)).replace("%id%", "pronunciation").replace("%label%",
                                                                                                              "Pronunciation"))
         f.write(
@@ -141,6 +174,9 @@ if len(word) > 0:
         f.write(HTML3_2.replace("%value%", str(etymology)).replace("%id%", "etymology").replace("%label%", "Etymology"))
 
         f.write(HTML2)
+
+    with open("./test.json", "w+", encoding="utf-8") as f:
+        f.write(str(details))
 
     print("\"" + text_to_find + "\" found and saved.")
 else:
